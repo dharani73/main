2fa9894af0cec0a64a348bc98f15cf023d1f3c8d
dharani73 <miriyala.dharani@gmail.com>
2022-11-28 22:05:14 +0530

    workingonedit

--- app/controllers/edit.js
+++ app/controllers/edit.js
@@ -1,11 +1,20 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+// import { tracked } from '@glimmer/tracking';
 export default class EditController extends Controller {
     @service store;
+
+    // @tracked title;
+    // @tracked homedelivery;
+    // @tracked location;
+    // @tracked rating;
+    // @tracked stock;
+    // @tracked image;
     @action
     async editPost(event){
         event.preventDefault();
+        console.log("hi");
         let edit=await this.model;
         edit.save().then (()=>{
             this.transitionToRoute('index');
--- app/routes/edit.js
+++ app/routes/edit.js
@@ -3,6 +3,6 @@ import { inject as service } from '@ember/service';
 export default class EditRoute extends Route {
     @service store;
   async model(params) {
-    return await this.store.findRecord('post', params.post_id);
+    return await this.store.findRecord('post', params.id);
   }
 }
